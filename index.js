be765e510af5533e46d69432719eddfd5f6f68a7
Md Sohrab Ansari <ansarisohrab75@gmail.com>
2023-08-15 16:06:59 +0530

    changes

--- index.js
+++ index.js
@@ -13,11 +13,25 @@ app.post('/orders', (req, res) => {
   const requestData = req.body;
    // Log the received data
   // Process requestData and respond accordingly
+  console.log("Received request:", req.headers);
   ordersInfo.push(requestData);
-  fs.writeFileSync('orders.json', JSON.stringify(ordersInfo, null, 2), 'utf-8');
-  // For example, you can send a response back to the client:
-  res.status(200).json({ message: 'Order received successfully', data: requestData });
-  console.log("Received data:", requestData);
+
+  // fs.writeFileSync('orders.json', JSON.stringify(ordersInfo, null, 2), 'utf-8');
+  // // For example, you can send a response back to the client:
+  // res.status(200).json({ message: 'Order received successfully', data: requestData });
+  // console.log("Received data:", requestData);
+
+  try {
+    // Process requestData and respond accordingly
+    ordersInfo.push(requestData);
+    fs.writeFileSync('orders.json', JSON.stringify(ordersInfo, null, 2), 'utf-8');
+
+    res.status(200).json({ message: 'Order received successfully', data: requestData });
+    console.log("Response sent.");
+  } catch (error) {
+    console.error("Error processing request:", error);
+    res.status(500).json({ message: 'Error processing request' });
+  }
 });
 
 app.get('/orders', (req, res) => {
